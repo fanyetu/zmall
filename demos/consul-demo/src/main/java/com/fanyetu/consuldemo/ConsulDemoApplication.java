da9b482a119a2dbbc58929243f7e6812f46eb022
fanyetu <hnan187@126.com>
2019-01-15 23:40:14 +0800

    consul demo

---
+++ demos/consul-demo/src/main/java/com/fanyetu/consuldemo/ConsulDemoApplication.java
@@ -0,0 +1,14 @@
+package com.fanyetu.consuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ConsulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulDemoApplication.class, args);
+	}
+
+}
+
