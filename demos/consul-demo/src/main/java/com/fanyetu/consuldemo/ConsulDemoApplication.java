eea92f9bc1318189f5f84cc8a5f7048d40cd8fb2
fanyetu <hnan187@126.com>
2019-01-16 23:55:31 +0800

    consul demo

--- demos/consul-demo/src/main/java/com/fanyetu/consuldemo/ConsulDemoApplication.java
+++ demos/consul-demo/src/main/java/com/fanyetu/consuldemo/ConsulDemoApplication.java
@@ -2,9 +2,11 @@ package com.fanyetu.consuldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 // 定时watch consul的配置，刷新本地配置
 @EnableScheduling
 public class  ConsulDemoApplication {
